c0d6b49448360e01a31ad34fc2889f7e8542f176
Tim Speckhals <timmy14085@gmail.com>
2017-11-17 12:38:01 +0000

    Create Example.php

---
+++ Example.php
@@ -0,0 +1,18 @@
+<?php
+require "DBAPI.class.php";
+
+$res = $db->query("SELECT * FROM `table` WHERE 1");
+foreach($res as $row)
+{
+    // $row is set for each dataset in the table `table`
+}
+
+$res = $db->query("SELECT * FROM `table` WHERE `this`=?", "s", "that");
+       // The second argument is for "string" and the following argument is a string to be replaced with the question mark (?)
+// $res is an array of all rows matching the query
+
+$res = $db-query("SELECT * FROM `table` WHERE `string`=? AND `int`=?", "si", "string", "1");
+       // In this case the second argument states 'the following two arguments are string (s) and int (i) respectively'
+       // The string ("string") and int (1) are to be placed in the query where the question marks (?) have been set
+       // This parsing of string and int excludes user data from any SQL execution and thereby blocks any injection attempts
+// $res again is an array of all rows matching the query
