dbbbe157bdd08364faaab574047d19401a56ac7d
Sainan <63328889+Sainan@users.noreply.github.com>
2021-02-19 18:34:03 +0100

    Add double to example

--- example.php
+++ example.php
@@ -15,7 +15,7 @@ $res = $db->query("SELECT * FROM `table` WHERE `this`=?", "s", "that");
        // The second argument is for "string" and the following argument is a string to be replaced with the question mark (?)
 // $res is an array of all rows matching the query
 
-$res = $db->query("SELECT * FROM `table` WHERE `string`=? AND `int`=?", "si", "string", "1");
+$res = $db->query("SELECT * FROM `table` WHERE `string`=? AND `int`=? AND `double`=?", "sid", "string", "1", 11.2);
        // In this case the second argument states 'the following two arguments are string (s) and int (i) respectively'
        // The string ("string") and int (1) are to be placed in the query where the question marks (?) have been set
        // This parsing of string and int excludes user data from any SQL execution and thereby blocks any injection attempts
